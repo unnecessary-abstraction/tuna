f2a649cf5515e2ec340bc9c9ff6eef8be29825ad
Paul Barker <paul@paulbarker.me.uk>
2013-03-20 18:12:21 +0000

    uara: Added sample_rate variable
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

--- src/uara.h
+++ src/uara.h
@@ -1,8 +1,8 @@
 /*******************************************************************************
-	uara.c: Main program entry point.
+	uara.h: Common declarations needed everywhere.
 
 	Copyright (C) 2013 Paul Barker, Loughborough University
-	
+
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation; either version 2 of the License, or
@@ -18,12 +18,11 @@
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 *******************************************************************************/
 
-#include "uara.h"
+#ifndef __UARA_H_INCLUDED__
+#define __UARA_H_INCLUDED__
 
-uint sample_rate;
+#include "types.h"
 
-int main(int argc, char * argv[])
-{
-	sample_rate = 625000;
-	return 0;
-}
+extern uint sample_rate;
+
+#endif /* !__UARA_H_INCLUDED__ */
