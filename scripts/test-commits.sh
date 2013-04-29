44fa27f887ebdbf5dfbcea94760fe5757aef27cd
Paul Barker <paul@paulbarker.me.uk>
2013-04-29 18:29:14 +0100

    scripts: Make test-commits.sh work in temp dir
    Rather than stepping through commits in the local directory which could cause
    breakage or leave an unsuspecting developer with a dangling head if things fail,
    the current directory is copied to a temp directory and the commit test is
    performed there.
    
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

--- scripts/test-commits.sh
+++ scripts/test-commits.sh
@@ -5,9 +5,20 @@
 set -e
 RANGE=$1
 
+# Lets not break the current working directory
+SOURCEDIR=`pwd`
+WORKINGDIR=`mktemp -d`
+trap 'rm -rf $WORKINGDIR' EXIT
+cd "$WORKINGDIR"
+
+rsync -a "$SOURCEDIR/" .
+
 for COMMIT in $(git log --reverse --format=format:%H $RANGE); do
     git checkout ${COMMIT} ;
     make clean 1>/dev/null || { echo "$COMMIT: Failed make clean";  break; }
     make all 1>/dev/null || { echo "$COMMIT: Failed make all";  break; }
     make test 1>/dev/null || { echo "$COMMIT: Failed make test";  break; }
 done
+
+cd "$SOURCEDIR"
+rm -rf "$WORKINGDIR"
