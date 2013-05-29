3143f4b19128d1bf414d3495304ac5f749987573
Paul Barker <paul@paulbarker.me.uk>
2013-05-29 16:06:51 +0100

    uara: Added code to main() to test new functionality
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

--- src/uara.c
+++ src/uara.c
@@ -18,13 +18,47 @@
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 *******************************************************************************/
 
-#include "uara.h"
+#include "analysis.h"
+#include "bufq.h"
 #include "compiler.h"
+#include "input_sndfile.h"
+#include "log.h"
+#include "output_csv.h"
+#include "uara.h"
 
 int main(int argc, char * argv[])
 {
 	__unused argc;
 	__unused argv;
+
+	struct producer * in;
+	struct consumer * bufq;
+	struct consumer * analysis;
+	struct consumer * out_impulse;
+	struct consumer * out_time_slice;
+
+	/* These need to be configurable. */
+	const char * source = "input.wav";
+	const char * sink_time_slice = "time_slice.csv";
+	const char * sink_impulse = "impulse.csv";
+	const char * log_file = "uara.log";
+	const char * app_name = "uara";
+	
+	log_init(log_file, app_name);
+
+	out_time_slice = output_csv_init(sink_time_slice);
+	out_impulse = output_csv_init(sink_impulse);
+	analysis = analysis_init(out_time_slice, out_impulse);
+	bufq = bufq_init(analysis);
+	in = input_sndfile_init(source, bufq);
+
+	in->run(in);
+
+	out_time_slice->exit(out_time_slice);
+	out_impulse->exit(out_impulse);
+	analysis->exit(analysis);
+	bufq->exit(bufq);
+	in->exit(in);
 
 	return 0;
 }
