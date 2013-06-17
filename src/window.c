7b62e6da78e90607905995cd9e320390bebc5239
Paul Barker <paul@paulbarker.me.uk>
2013-06-17 13:11:00 +0100

    window (new): Added sine window function generator
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

---
+++ src/window.c
@@ -0,0 +1,36 @@
+/*******************************************************************************
+	window.c: Window function.
+
+	Copyright (C) 2013 Paul Barker, Loughborough University
+	
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*******************************************************************************/
+
+#include <assert.h>
+#include <math.h>
+
+#include "types.h"
+#include "window.h"
+
+void window_init_sine(double * window, uint length)
+{
+	uint i;
+
+	assert(window);
+
+	for (i = 0; i < length; i++) {
+		window[i] = 2 * sin(M_PI * i / length);
+	}
+}
---
+++ src/window.h
@@ -0,0 +1,28 @@
+/*******************************************************************************
+	window.h: Window functions.
+
+	Copyright (C) 2013 Paul Barker, Loughborough University
+	
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*******************************************************************************/
+
+#ifndef __UARA_WINDOW_H_INCLUDED__
+#define __UARA_WINDOW_H_INCLUDED__
+
+#include "types.h"
+
+void window_init_sine(double * window, uint length);
+
+#endif /* !__UARA_WINDOW_H_INCLUDED__ */
