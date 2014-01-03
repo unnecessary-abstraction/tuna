9ced20bbf8df8ffa04a9ea7bbcea0c3a2406425d
Paul Barker <paul@paulbarker.me.uk>
2014-01-03 20:46:26 +0000

    cbuf (new): Circular sample buffer
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

---
+++ include/tuna/cbuf.h
@@ -0,0 +1,41 @@
+/*******************************************************************************
+	cbuf.h: Circular buffering.
+
+	Copyright (C) 2013,2014 Paul Barker, Loughborough University
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*******************************************************************************/
+
+#ifndef __TUNA_CBUF_H_INCLUDED__
+#define __TUNA_CBUF_H_INCLUDED__
+
+#include "types.h"
+
+struct cbuf {
+	uint index;
+	uint len;
+
+	sample_t data[];
+};
+
+struct cbuf * cbuf_init(uint len);
+void cbuf_reset(struct cbuf * c);
+sample_t cbuf_index(struct cbuf * c, uint i);
+sample_t cbuf_get(struct cbuf * c);
+void cbuf_put(struct cbuf * c, sample_t s);
+sample_t cbuf_rotate(struct cbuf *c, sample_t s);
+void cbuf_exit(struct cbuf * c);
+
+#endif /* !__TUNA_CBUF_H_INCLUDED__ */
---
+++ libtuna/cbuf.c
@@ -0,0 +1,95 @@
+/*******************************************************************************
+	cbuf.c: Circular buffering.
+
+	Copyright (C) 2013,2014 Paul Barker, Loughborough University
+	
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*******************************************************************************/
+
+#include <assert.h>
+#include <malloc.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "cbuf.h"
+#include "log.h"
+#include "types.h"
+
+struct cbuf * cbuf_init(uint len)
+{
+	/* Use calloc so that the index and all data values are initialised to
+	 * zero.
+	 */
+	struct cbuf * c = calloc(1, sizeof(struct cbuf) + len * sizeof(sample_t));
+	if (!c) {
+		error("cbuf_init: Failed to allocate memory");
+		return NULL;
+	}
+
+	c->len = len;
+
+	return c;
+}
+
+void cbuf_reset(struct cbuf * c)
+{
+	assert(c);
+
+	size_t sz = sizeof(struct cbuf) + c->len * sizeof(sample_t);
+
+	memset(c, 0, sz);
+}
+
+sample_t cbuf_index(struct cbuf * c, uint i)
+{
+	assert(c);
+
+	return c->data[(c->index + i) % c->len];
+}
+
+sample_t cbuf_get(struct cbuf * c)
+{
+	assert(c);
+
+	return c->data[c->index];
+}
+
+void cbuf_put(struct cbuf * c, sample_t s)
+{
+	assert(c);
+
+	c->data[c->index] = s;
+	c->index = (c->index + 1) % c->len;
+}
+
+sample_t cbuf_rotate(struct cbuf *c, sample_t s)
+{
+	assert(c);
+
+	sample_t tmp;
+
+	tmp = c->data[c->index];
+	c->data[c->index] = s;
+	c->index = (c->index + 1) % c->len;
+
+	return tmp;
+}
+
+void cbuf_exit(struct cbuf * c)
+{
+	assert(c);
+
+	free(c);
+}
